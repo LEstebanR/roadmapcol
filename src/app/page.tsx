dc650694c590f0f2a113901431efe826a2703735
Luis Esteban Ramírez <lesteban@Luiss-Mac-mini.local>
2025-06-28 12:35:43 -0500

    feat: update landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { CarouselHome } from '@/components/ui/carousel-home'
 
 export default function Home() {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-4">
+    <div className="flex w-full flex-col items-center justify-center gap-4 p-0">
       <CarouselHome />
     </div>
   )
